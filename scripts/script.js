2006335baf73b9051ec453303c21db5f76c7e9ea
mohammad reza <mrghkh12@gmail.com>
2024-07-21 15:09:41 +0330

    style: clean codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-const $ = document
+const $ = document;
 
-let showPopupBtn = $.querySelector('.show-btn')
-let popupElem = $.querySelector('.popupElem')
+let showPopupBtn = $.querySelector(".show-btn");
+let popupElem = $.querySelector(".popupElem");
 
-let clossPopupBtn = $.querySelector('.clossPopup')
+let clossPopupBtn = $.querySelector(".clossPopup");
 
-showPopupBtn.addEventListener('click' , () => {
-    popupElem.classList.add('open-popup')
-})
+showPopupBtn.addEventListener("click", () => {
+  popupElem.classList.add("open-popup");
+});
 
-clossPopupBtn.addEventListener('click' , () => {
-    popupElem.classList.remove('open-popup')
-})
+clossPopupBtn.addEventListener("click", () => {
+  popupElem.classList.remove("open-popup");
+});
