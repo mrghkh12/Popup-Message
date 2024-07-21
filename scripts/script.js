6a4ae5a3116831afac0ccf680c7bf8df7cca67de
mohammad reza <mrghkh12@gmail.com>
2024-07-21 14:18:58 +0330

    feat(script.js): add event open & closs popup

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,11 @@ let showPopupBtn = $.querySelector('.show-btn')
 let popupElem = $.querySelector('.popupElem')
 
 let clossPopupBtn = $.querySelector('.clossPopup')
+
+showPopupBtn.addEventListener('click' , () => {
+    popupElem.classList.add('open-popup')
+})
+
+clossPopupBtn.addEventListener('click' , () => {
+    popupElem.classList.remove('open-popup')
+})
